c30fb1a3e2f9f61781f8e83c3d489e3182f34d26
Navinder Brar <navinder_brar@yahoo.com>
2019-12-27 21:07:19 +0530

    Run kafka locally

---
+++ start.sh
@@ -0,0 +1,4 @@
+./bin/zookeeper-server-start.sh ./config/zookeeper.properties &
+./bin/kafka-server-start.sh ./config/server1.properties &
+./bin/kafka-server-start.sh ./config/server2.properties &
+./bin/kafka-server-start.sh ./config/server3.properties &
